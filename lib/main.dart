1226f886a7b12bc053e2f6da77edc5ebc27cf2fa
Carlos Rocha <crocha09.09@gmail.com>
2023-09-07 22:06:14 -0600

    adds apply repository layer and datasource all project

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:tokto_app/config/theme/app_theme.dart';
+import 'package:tokto_app/infrastruture/datasources/local_video_datasource_impl.dart';
+import 'package:tokto_app/infrastruture/repositories/video_posts_repository_impl.dart';
 import 'package:tokto_app/presentation/screens/discover/discover_screen.dart';
 import 'package:tokto_app/providers/discover_provider.dart';
 
@@ -14,11 +16,12 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
+    final videoPostRepository = VideoPostsRepositoryImpl(videosDatasource: LocalVideoDatasource());
     return MultiProvider(
       providers: [
         ChangeNotifierProvider(
           lazy: false,
-          create: (_) => DiscoverProvider()..loadNextPage() ),
+          create: (_) => DiscoverProvider(videoRepository: videoPostRepository)..loadNextPage() ),
       ],
       child: MaterialApp(
         title: 'toktok app',
--- lib/providers/discover_provider.dart
+++ lib/providers/discover_provider.dart
@@ -1,23 +1,26 @@
 
 import 'package:flutter/material.dart';
 import 'package:tokto_app/domain/entities/video_post.dart';
+import 'package:tokto_app/domain/repositories/video_posts_repository.dart';
 import 'package:tokto_app/infrastruture/models/local_video_model.dart';
 import 'package:tokto_app/shared/data/local_video_posts.dart';
 
 class DiscoverProvider extends ChangeNotifier {
 
-  // TODO:repository, datasource
+  final VideoPostRepository videoRepository;
 
   bool initialLoading = true;
   List<VideoPost> videos = [];
 
+  DiscoverProvider({required this.videoRepository});
+
   Future<void> loadNextPage() async {
 
     // await Future.delayed( const Duration(seconds: 2) );
-
-    final List<VideoPost> newVideos = videoPosts.map( 
-      ( video ) => LocalVideoModel.fromJson(video).toVideoPostEntity() 
-      ).toList();
+    // final List<VideoPost> newVideos = videoPosts.map( 
+    //   ( video ) => LocalVideoModel.fromJson(video).toVideoPostEntity() 
+    //   ).toList();
+    final newVideos = await videoRepository.getTrendingVideosByPage(1);
 
     videos.addAll(newVideos);
     initialLoading = false;
